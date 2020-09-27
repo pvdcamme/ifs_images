0ca629be3748efbc40cf46c18d0cf079a24640aa
Pieter Vandercammen <pieter.vandercammen@excentis.com>
2020-09-27 16:54:19 +0200

    Cleanup code a bit

--- stats.py
+++ stats.py
@@ -5,17 +5,13 @@ import numpy
 import sys
 import re
 
+
 def keep_number(rr):
     gg = re.search('\d+(\.\d*)?', rr)
     if gg:
         return float(gg.group(0))
-    return 0        
+    return 0
 
-with open(sys.argv[1],'r') as f:
-    data = list(map(keep_number, f))
-
-to_show = [('Min', 'Avg', 'Max', 'std'),
-           (min(data), numpy.average(data), max(data), numpy.std(data))] 
 
 def pretty_show(rows):
     widest_colum = 0
@@ -25,9 +21,19 @@ def pretty_show(rows):
         all_txt.append(txt_row)
         widest_colum = max(widest_colum, *map(len, txt_row))
 
-    
     for txt_row in all_txt:
-        str_row = '|'.join(map(lambda cc: cc.center(widest_colum),txt_row))
+        str_row = '|'.join(map(lambda cc: cc.center(widest_colum), txt_row))
         print(str_row)
 
-pretty_show(to_show)
+
+def load_data(file_name):
+    with open(file_name, 'r') as f:
+        data = list(map(keep_number, f))
+    return numpy.array(data)
+
+
+if '__main__' == __name__:
+    data = load_data(sys.argv[1])
+    to_show = [('Min', 'Avg', 'Max', 'std'), (min(data), numpy.average(data),
+                                              max(data), numpy.std(data))]
+    pretty_show(to_show)
