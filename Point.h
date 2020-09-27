808ac3e9647c53f76091fff0f0295194ad38d424
Pieter Vandercammen <pieter.vandercammen@excentis.com>
2020-09-27 18:36:21 +0200

    Investigate SIMD approach, begin with a Point

--- Point.h
+++ Point.h
@@ -1,6 +1,8 @@
 #ifndef POINT_H
 #define POINT_H
 
+#include <immintrin.h>
+
 struct Point {
     float x,y;
 
@@ -8,5 +10,30 @@ struct Point {
         x(_x), y(_y)
     {}
 };
+
+/* Stores several points together.
+ * This class is intended for an SIMD approach.
+ */
+template<typename vector_type>
+struct MultiPoint {
+    vector_type x;
+    vector_type y;
+
+    MultiPoint(float init_x, float init_y){
+        x = x ^ x + init_x;
+        y = y ^ y + init_y;
+
+    }
+
+    size_t size(){
+        return sizeof(vector_type) / sizeof(float);
+    }
+
+    Point operator[](size_t idx){
+        return Point(x[idx], y[idx]);
+    }
+};
+
+
 
 #endif
