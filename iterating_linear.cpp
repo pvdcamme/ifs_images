80bc47bec799113a8009b840f40de8ebd5f383e4
pvdcamme <pvdcamme@gmail.com>
2022-08-15 10:56:07 +0000

    Auto formatting

--- iterating_linear.cpp
+++ iterating_linear.cpp
@@ -25,7 +25,7 @@
 
    @detail.
    Most small values are passed by-value rather then by reference.
-   This gives us nice-pure functions that are easy to reason 
+   This gives us nice-pure functions that are easy to reason
    about. Benchmarking has shown no performance degradation.
 
 */
@@ -64,15 +64,15 @@ struct Linear {
                      d* p.x + e* p.y + f,
                      id);
     }
-    
+
     /* Performs multiple transforms simultaneously.
      * Offers a nice speedup, despite the gathering.
      */
     static MultiPoint move(MultiPoint pp,
-                const Linear& l1,
-                const Linear& l2,
-                const Linear& l3,
-                const Linear& l4)
+                           const Linear& l1,
+                           const Linear& l2,
+                           const Linear& l3,
+                           const Linear& l4)
     {
         float_vector_type aa = {l1.a, l2.a, l3.a, l4.a};
         float_vector_type bb = {l1.b, l2.b, l3.b, l4.b};
@@ -80,7 +80,7 @@ struct Linear {
         float_vector_type dd = {l1.d, l2.d, l3.d, l4.d};
         float_vector_type ee = {l1.e, l2.e, l3.e, l4.e};
         float_vector_type ff = {l1.f, l2.f, l3.f, l4.f};
-        
+
         float_vector_type x = aa * pp.x + bb * pp.y + cc;
         float_vector_type y = dd * pp.x + ee * pp.y + ff;
         int_vector_type z = {l1.id, l2.id, l3.id, l4.id};
@@ -91,16 +91,16 @@ struct Linear {
 
 /**
  A fast power operation.
- Delegates much to the compiler, use 
+ Delegates much to the compiler, use
  only for small exponents.
 */
 template<size_t exp>
-size_t pow(size_t val){
+size_t pow(size_t val) {
     return val * pow<exp -1>(val);
 }
 
 template<>
-size_t pow<0>(size_t val){
+size_t pow<0>(size_t val) {
     return 1;
 }
 
@@ -113,9 +113,9 @@ struct TransformGroup {
     Linear transforms[transform_count];
 
     TransformGroup() {
-      for(auto ctr(0); ctr < transform_count; ++ctr){
-        transforms[ctr].id =ctr;
-      }
+        for(auto ctr(0); ctr < transform_count; ++ctr) {
+            transforms[ctr].id =ctr;
+        }
     }
 
     Point move(Point p) const {
@@ -131,10 +131,10 @@ struct TransformGroup {
         size_t idx3 = (rr /pow<3>(transform_count)) % transform_count;
 
         return Linear::move(p,
-                    transforms[idx0],
-                    transforms[idx1],
-                    transforms[idx2],
-                    transforms[idx3]);
+                            transforms[idx0],
+                            transforms[idx1],
+                            transforms[idx2],
+                            transforms[idx3]);
     }
 };
 
@@ -157,7 +157,6 @@ int main(int argc, char** argv) {
     if(argc > 1) {
         target_name = argv[1];
     }
-
     init_seed();
 
     World<1024,1> w;
